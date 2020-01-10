0d0ff43423e2049046ac23d10e3ea37d34a955ad
m01i0ng <alley.ma@qq.com>
2020-01-10 19:40:14 +0800

    Update openfile.rb fix sha256

--- Casks/openfile.rb
+++ Casks/openfile.rb
@@ -1,6 +1,6 @@
 cask 'openfile' do
     version '1.3.1'
-    sha256 '8733ea3fea31b55b39564f78c1d3a24f7ec41ef712471428e7f76a2f79a2b4b'
+    sha256 '8733ea3fea31b55b39564f78c1d3a24f7ec41ef712471428e7f76a2f79a2b4b2'
   
     url "https://github.com/nlnlnull/OpenFile/releases/download/V#{version}/OpenFile.zip"
     appcast 'https://github.com/nlnlnull/OpenFile/releases.atom'
